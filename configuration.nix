f3ff944a155c68dd61e9dfb318f7e2b68bdb8669
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-04-09 18:25:45 +0200

    .

--- configuration.nix
+++ configuration.nix
@@ -217,6 +217,8 @@ in
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    direnv
+    openjdk
     stow
     gitFull
     git-lfs
